a2012304bc5773cde054f7673dfcf2e545c8aa35
danielgbaptistone <dgbaptistone@hotmail.com>
2024-09-23 21:36:48 -0300

    ex015

--- desafio/ex0015.py
+++ desafio/ex0015.py
@@ -1,4 +1,4 @@
 d = int(input('Quantos dias alugados? '))
-k = float(input('Quantos Km rodados?'))
+k = float(input('Quantos Km rodados? km'))
 vs = (d*60)+(k*0.15)
 print('Valor a pagar do aluguel é de R${:.2f}'.format(vs))
