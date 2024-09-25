d7c7b245550fc397e63158d15330770205d99bc4
danielgbaptistone <dgbaptistone@hotmail.com>
2024-09-24 22:12:40 -0300

    ex17revi

--- desafio/ex017.py
+++ desafio/ex017.py
@@ -3,4 +3,4 @@ cato = float(input('Entre com o valor do cateto oposto: '))
 cata = float(input('Entre com o Valor do Cateto adjacente: '))
 hipo = math.hypot(cato, cata)
 print('A soma do cateto oposto {} com cateto adjacente {} igual a hipotenusa {} !!'.format(
-    cato, cata, hipo))
+    cato, cata, math.trunc(hipo)))
