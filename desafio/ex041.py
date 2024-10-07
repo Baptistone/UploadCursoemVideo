f360f57aa9910cdfb440181b46c16fa1fcb08041
danielgbaptistone <dgbaptistone@hotmail.com>
2024-10-07 19:04:59 -0300

    modified:   desafio/ex041.py 	new file:   desafio/ex042.py

--- desafio/ex041.py
+++ desafio/ex041.py
@@ -1,3 +1,15 @@
+from datetime import date
 ano = int(input('Ano de nascimento: '))
-
-print = ('O Nadador tem {} anos'.format())
+atual = date.today().year
+idade = atual - ano
+print('O Nadador tem {} anos'.format(idade))
+if idade <= 9:
+    print('Classificação: MIRIM')
+elif idade >= 10 and idade <= 14:
+    print('Classificação: INFANTIL')
+elif idade >= 15 and idade <= 19:
+    print('Classificação: JUNIOR')
+elif idade >= 20 and idade <= 25:
+    print('Classificação: SENIOR')
+else:
+    print('Classificação: MASTER')
