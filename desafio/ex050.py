144692b53b8075342bdc6f85de220884c6378c27
danielgbaptistone <dgbaptistone@hotmail.com>
2024-10-09 22:09:39 -0300

    modified:   desafio/ex050.py

--- desafio/ex050.py
+++ desafio/ex050.py
@@ -1,8 +1,8 @@
-n1 = int(input('Entre com valor: '))
-n2 = int(input('Entre com valor: '))
-n3 = int(input('Entre com valor: '))
-n4 = int(input('Entre com valor: '))
-n5 = int(input('Entre com valor: '))
-n6 = int(input('Entre com valor: '))
-for c in range(1, 6, 2):
-    print('1')
+soma = 0
+cont = 0
+for c in range(1, 7):
+    n = int(input('Digite o valor {}: '.format(c)))
+    if n % 2 == 0:
+        soma += n
+        cont += 1
+print('Voce informou {} numeros e a soma deles foi {}'.format(cont,soma))
